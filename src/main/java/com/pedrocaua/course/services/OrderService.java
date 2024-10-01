ee419c54f62f7420429567de4896958f596fe8e9
Pedro Caua <pedro.cauasalgado@gmail.com>
2024-10-01 12:23:43 -0300

    Entidade Pedido, Datas com Instant e Padrão ISO 8601 - Parte 02

---
+++ src/main/java/com/pedrocaua/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.pedrocaua.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrocaua.course.entities.Order;
+import com.pedrocaua.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
