43bb3d776b2366f9cc11888236ec27340b21e8dc
Pedro Caua <pedro.cauasalgado@gmail.com>
2024-10-04 15:24:24 -0300

    Métodos Subtotal e Total

--- src/main/java/com/pedrocaua/course/entities/Order.java
+++ src/main/java/com/pedrocaua/course/entities/Order.java
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
@@ -116,5 +124,5 @@ public class Order implements Serializable {
 			return false;
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
-	}
+	}	
 }
--- src/main/java/com/pedrocaua/course/entities/OrderItem.java
+++ src/main/java/com/pedrocaua/course/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return quantity * price;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -80,8 +84,5 @@ public class OrderItem implements Serializable {
 			return false;
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
-	}
-	
-	
-	
+	}	
 }
